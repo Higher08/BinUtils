a4bbddaa271b70471749046e4d819e7a0a70f084
Higher08 <68398868+Higher08@users.noreply.github.com>
2022-02-10 16:29:14 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,20 @@ import PackageDescription
 let package = Package(
     name: "BinUtils",
     products: [
-        .library(name: "FDB", targets: ["FDB"]),
+        .library(name: "BinUtils", targets: ["BinUtils"]),
     ],
+)
+
+let package = Package(
+    name: "BinUtils",
+    platforms: [
+       .macOS(.v10_15)
+    ],
+    products: [
+        .library(name: "BinUtils", targets: ["BinUtils"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "BinUtils", dependencies: []),
+    ]
 )
